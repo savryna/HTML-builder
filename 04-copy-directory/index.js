18f880a94c2a929b4791029a28305d4b95f1175a
Anastasia Savrukhina <assavr0@gmail.com>
2025-01-20 22:33:06 +0300

    refactor: split into two functions for 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,25 +1,30 @@
 const fsPromises = require('node:fs/promises');
 const path = require('node:path');
 
-(async () => {
-  const sourceFolderPath = path.join(__dirname, 'files');
-  const copyFolderPath = path.join(__dirname, 'copyFiles');
-  
-  try {
- 
-    try {
-      await fsPromises.access(copyFolderPath);
-      await fsPromises.rm(copyFolderPath, { recursive: true, force: true });
-    } catch (err) {
-        console.error(err);
-    }
+const sourceFolderPath = path.join(__dirname, 'files');
+const copyFolderPath = path.join(__dirname, 'copyFiles');
 
-    await fsPromises.mkdir(copyFolderPath, {recursive: true});
-    const sourceFilesName = ((await fsPromises.readdir(sourceFolderPath)));
+async function copyFiles(sourceFolder, copyFolder) {
+  try {
+    await checkExists(copyFolder);
+    await fsPromises.mkdir(copyFolder, {recursive: true});
+    const sourceFilesName = ((await fsPromises.readdir(sourceFolder)));
     for (let i = 0; i < sourceFilesName.length; i++) {
-      await fsPromises.copyFile(path.join(sourceFolderPath, sourceFilesName[i]), path.join(copyFolderPath, sourceFilesName[i]))
+      await fsPromises.copyFile(path.join(sourceFolder, sourceFilesName[i]), path.join(copyFolder, sourceFilesName[i]))
     }
   } catch (err) {
     console.error(err);
   }
-})()
+}
+
+async function checkExists(checkPath) {
+  try {
+    await fsPromises.access(checkPath);
+    await fsPromises.rm(checkPath, { recursive: true, force: true });
+  } catch (err) {
+      await fsPromises.mkdir(checkPath, {recursive: true});
+      // console.error(err);
+  }
+}
+
+copyFiles(sourceFolderPath, copyFolderPath)
